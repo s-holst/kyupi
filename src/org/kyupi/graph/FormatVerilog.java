685fb761f570f3008a0058be7179f520aa1a71d0
Stefan Holst <mail@s-holst.de>
2017-04-20 10:45:26 +0900

    remove broken and deprecated code

--- src/org/kyupi/graph/FormatVerilog.java
+++ src/org/kyupi/graph/FormatVerilog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 The KyuPI project contributors. See the COPYRIGHT.md file
+ * Copyright 2013-2017 The KyuPI project contributors. See the COPYRIGHT.md file
  * at the top-level directory of this distribution.
  * This file is part of the KyuPI project. It is subject to the license terms
  * in the LICENSE.md file found in the top-level directory of this distribution.
@@ -30,47 +30,7 @@ public class FormatVerilog {
 		ArrayList<Graph> units = tree.elaborateAll(library);
 		if (units.size() == 0)
 			return new Graph(library);
-
-		Graph g = units.get(0);
-		
-		// some output ports may drive other nodes in the graph.
-		// re-wire them.
-
-		Node intf[] = g.accessInterface();
-
-		boolean doIterate = true;
-
-		while (doIterate) {
-			doIterate = false;
-			for (Node output : intf) {
-				if (output == null || !output.isOutput())
-					continue;
-				for (int oidx = output.maxOut(); oidx >= 0; oidx--) {
-					doIterate = true;
-					Node successor = output.out(oidx);
-					if (successor != null) {
-						Node predecessor = output.in(0);
-						if (predecessor == null) {
-							log.error("rewire failed: output " + output.queryName() + " has no driver");
-							doIterate = false;
-							break;
-						}
-						if (predecessor.isMultiOutput()) {
-							Node signal = g.new Node(output.queryName() + "_net", Library.TYPE_BUF | Library.FLAG_PSEUDO);
-							int opin = predecessor.searchOutIdx(output);
-							g.connect(predecessor, opin, signal, -1);
-							g.connect(signal, -1, output, 0);
-							predecessor = signal;
-						}
-						int iidx = successor.searchInIdx(output);
-						g.connect(predecessor, -1, successor, iidx);
-						output.setOut(oidx, null);
-					}
-				}
-			}
-		}
-
-		return g;
+		return units.get(0);
 	}
 	
 	private static String s(String raw) {
@@ -80,15 +40,15 @@ public class FormatVerilog {
 	}
 	
 	/**
-	 * FIXME do verilog dump.
+	 * Saving graph in verilog format to given output stream.
 	 * 
 	 * @param os
 	 * @param graph
 	 * @param entity_name
 	 */
-	public static void save(OutputStream os, Graph graph, String entity_name) {
+	public static void save(OutputStream os, Graph graph) {
 		PrintWriter op = new PrintWriter(os);
-		op.println("module " + entity_name + " ( ");
+		op.println("module " + graph.getName() + " ( ");
 		boolean comma_needed = false;
 		for (Node intf_node : graph.accessInterface()) {
 			if (intf_node == null)
